c398aef52d371f25993d5ed2186eb96dca091a32
Cristian Daniel Gutiérrez S. <cdgutierrez6@gmail.com>
2026-05-26 13:37:40 -0500

    fix(langchain): use langchain_text_splitters package (langchain 0.3+ split)

--- app/core/document_loader.py
+++ app/core/document_loader.py
@@ -3,7 +3,7 @@ import tempfile
 from typing import List
 from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from app.core.config import settings
 
 
